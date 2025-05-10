85f376b3a35ef6f6470a231cb13bf8d112f5062e
adeloid <ada.not@gmail.com>
2025-05-10 03:54:50 +0300

    fix: return userName

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,8 @@ import readlineSync from 'readline-sync';
 const getUserName = () => {
     console.log("Welcome to the Brain Games!");
     const userName = readlineSync.question('May I have your name? ');
-    return console.log(`Hello, ${userName}!`);
+    console.log(`Hello, ${userName}!`);
+    return userName;
 }
 
 export default getUserName;
